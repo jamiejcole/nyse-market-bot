ca9ef37a232e2fc2bc429ffe5121b376cf09d5cf
tobsterclark <tobsterclark@gmail.com>
2021-02-01 14:19:10 +1100

    First edit

---
+++ README.md
@@ -0,0 +1,12 @@
+# NYSE Market bot
+A simple python bot for predicting and purchasing NYXBC stocks.
+
+
+### 🚀 Used technologies
+Matplotlib, sklearn, numpy & pandas
+
+### 📝 TODO
+- Checkout the [trello board](https://trello.com/b/2sVgv5zr/ipt-at1-timeline)
+
+### ⚠️ Disclaimer
+This is a simple school project playing with pretend money, not meant to be used for actual financial gain.
---
+++ test.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import quandl
+import datetime
+import pylab
+import matplotlib.pyplot as plt
+
+start = datetime.datetime(2018,1,1)
+end = datetime.date.today()
+quandl.ApiConfig.api_key = "_mEXfKTb2sgScinopKpX"
+ 
+# Let's get Apple stock data; Apple's ticker symbol is AAPL
+# First argument is the series we want, second is the source ("yahoo" for Yahoo! Finance), third is the start date, fourth is the end date
+
+data = quandl.get("NASDAQOMX/NQAU60AUD", start_date=start, end_date=end)
+type(data)
+
+
+pylab.rcParams['figure.figsize'] = (15, 9)
+data["Index Value"].plot(grid = True)
+
+plt.show()
